01e1c4e9e6fafa5bbdbe2f5291e3ff52601f7980
Ailyn Aguirre <ailyndarlenne@gmail.com>
2023-03-16 16:35:10 -0600

    Class page using dictionary of info

--- src/components/Classes.js
+++ src/components/Classes.js
@@ -9,72 +9,108 @@ import tempClass3 from "../assets/temp-classes3.jpeg";
 import tempClass4 from "../assets/temp-classes4.jpeg";
 import tempClass5 from "../assets/temp-classes5.jpeg";
 
-// function afterr() {
-//   document.getElementById("myImage").src = "img/photo2.jpg";
-//   document.getElementById("message").innerHTML = "Bye! GeeksforGeeks people";
-// }
 function Classes() {
-  // const [active, setActive] = useState(false);
-  // const handleClick = () => {
-  //   setActive(!active);
-  // };
+  const setClassInfo = (curClass) => {
+    var rgstrRqd = "Registration Required";
+    var dropInWlcm = "Drop-Ins Welcome";
 
-  const balletI = () => {
-    document.getElementById("cur-class-image").src = tempClass1;
+    var classInfo = {
+      preBallet: {
+        classImg: tempClass3,
+        classSched: "Saturday 9:30 - 10:30 AM",
+        classDesc: "Designed for 3-5 year olds.",
+        classRgstr: rgstrRqd,
+      },
+      ballet1: {
+        classImg: tempClass1,
+        classSched: "Monday & Wednesday 4:30 - 5:30 PM",
+        classDesc:
+          "Designed for 6 - 8 year olds, this class teaches the elements of classical ballet. The course introduces basic terminology and training.",
+        classRgstr: rgstrRqd,
+      },
+      ballet2: {
+        classImg: tempClass2,
+        classSched: "Tuesday & Thursday 4:30 - 5:30pm",
+        classDesc: "Designed TODO BALLET II",
+        classRgstr: rgstrRqd,
+      },
+      ballet3: {
+        classImg: tempClass3,
+        classSched: "Monday & Wednesday 5:30 - 7:00pm",
+        classDesc: "Designed TODO BALLET III",
+        classRgstr: rgstrRqd,
+      },
+      ballet4: {
+        classImg: tempClass4,
+        classSched: "Tuesday & Thursday 5:30 - 7:00pm",
+        classDesc: "Designed TODO BALLET IV",
+        classRgstr: rgstrRqd,
+      },
+      pointe1and2: {
+        classImg: tempClass5,
+        classSched: "Fridays 4:30 & 5:30 PM",
+        classDesc: "Designed TODO Pointe I/II",
+        classRgstr: rgstrRqd,
+      },
+      adultBallet: {
+        classImg: tempClass4,
+        classSched: "Tuesday & Thursday 5:30 - 7:00pm",
+        classDesc: "Designed TODO Adult Ballet",
+        classRgstr: dropInWlcm,
+      },
+      flexibility: {
+        classImg: tempClass2,
+        classSched: "Monday & Wednesday 4:30 - 5:30pm. Saturday 10:30 - 11:30",
+        classDesc: "Designed TODO Flexibility",
+        classRgstr: dropInWlcm,
+      },
+    };
+
+    document.getElementById("cur-class-image").src =
+      classInfo[curClass].classImg;
     document.getElementById("cur-class-schedule").innerHTML =
-      "Monday & Wednesday 4:30 - 5:30pm";
+      classInfo[curClass].classSched;
+    document.getElementById("cur-class-rgstr").innerHTML =
+      classInfo[curClass].classRgstr;
     document.getElementById("cur-class-description").innerHTML =
-      "Designed for 6 - 8 year olds, this class teaches the elements of classical ballet. The course introduces basic terminology and training.";
-  };
-
-  const balletII = () => {
-    document.getElementById("cur-class-image").src = tempClass2;
-    document.getElementById("cur-class-description").innerHTML =
-      "TODO Description for balletII";
-  };
-
-  const balletIII = () => {
-    document.getElementById("cur-class-image").src = tempClass3;
-    document.getElementById("cur-class-description").innerHTML =
-      "TODO Description for ballet III";
-  };
-
-  const balletIV = () => {
-    document.getElementById("cur-class-image").src = tempClass4;
-    document.getElementById("cur-class-description").innerHTML =
-      "TODO Description for ballet IV";
-  };
-
-  const pointeIAndII = () => {
-    document.getElementById("cur-class-image").src = tempClass5;
-    document.getElementById("cur-class-description").innerHTML =
-      "TODO Description for pointe i and 2";
-  };
-
-  const flexibilityAndConditioning = () => {
-    document.getElementById("cur-class-image").src = tempClass1;
-    document.getElementById("cur-class-description").innerHTML =
-      "TODO Description for flexibility and conditioning";
+      classInfo[curClass].classDesc;
   };
 
   return (
     <div className="classes-container">
       <div className="left-listings">
         <div className="class-buttons">
-          <NyliaButton handleClick={balletI} label={"Ballet I"}></NyliaButton>
-          <NyliaButton handleClick={balletII} label={"Ballet II"}></NyliaButton>
           <NyliaButton
-            handleClick={balletIII}
+            handleClick={() => setClassInfo("preBallet")}
+            label={"Pre-Ballet"}
+          ></NyliaButton>
+          <NyliaButton
+            handleClick={() => setClassInfo("ballet1")}
+            label={"Ballet I"}
+          ></NyliaButton>
+          <NyliaButton
+            handleClick={() => setClassInfo("ballet2")}
+            label={"Ballet II"}
+          ></NyliaButton>
+          <NyliaButton
+            handleClick={() => setClassInfo("ballet3")}
             label={"Ballet III"}
           ></NyliaButton>
-          <NyliaButton handleClick={balletIV} label={"Ballet IV"}></NyliaButton>
           <NyliaButton
-            handleClick={pointeIAndII}
+            handleClick={() => setClassInfo("ballet4")}
+            label={"Ballet IV"}
+          ></NyliaButton>
+          <NyliaButton
+            handleClick={() => setClassInfo("pointe1and2")}
             label={"Pointe I/II"}
           ></NyliaButton>
           <NyliaButton
-            handleClick={flexibilityAndConditioning}
-            label={"Flexibility"}
+            handleClick={() => setClassInfo("adultBallet")}
+            label={"Adult Ballet"}
+          ></NyliaButton>
+          <NyliaButton
+            handleClick={() => setClassInfo("flexibility")}
+            label={"flexibility"}
           ></NyliaButton>
         </div>
       </div>
@@ -86,9 +122,8 @@ function Classes() {
         </div>
         <div className="class-info">
           <div className="class-schedule">
-            <p id="cur-class-schedule">
-              Mondays & Wednesdays <br></br>4:30 - 5:30 PM
-            </p>
+            <p id="cur-class-schedule">Mondays & Wednesdays 4:30 - 5:30 PM</p>
+            <p id="cur-class-rgstr">todo</p>
           </div>
           <div className="class-description">
             <p id="cur-class-description">
