ca7ab344498dcd4facf933353d56a5c26b2b282e
Ailyn Aguirre <ailyndarlenne@gmail.com>
2024-05-19 21:04:22 -0600

    Update index

--- src/components/UpcomingEvents.js
+++ src/components/UpcomingEvents.js
@@ -95,7 +95,7 @@ import PhotoGallery from "./photoGallery/PhotoGallery.js";
 import "./UpcomingEvents.css";
 
 function UpcomingEvents() {
-  const CUREVENTIDX = 11;
+  const CUREVENTIDX = 12;
   const images = [
     {
       smImg: newYear2024Sm,
