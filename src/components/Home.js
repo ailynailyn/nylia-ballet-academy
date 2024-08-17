aa380b3d06fe8935861bd445a74e77170cd32804
Ailyn Aguirre <ailyndarlenne@gmail.com>
2024-08-16 22:49:11 -0600

    Update home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -49,42 +49,9 @@ function Home() {
       </div>
 
       <div className="description">
-        <p>
-          At Nylia, we focus on large scale productions to widely demonstrate
-          how ballet operates as an art form that tells stories. Yet, the power
-          of art is not accomplishable without high quality technical training.
-          We want our dancers, and dancers throughout the city, to always be
-          developing their craft. Thus, we are thrilled to announce auditions
-          for our premier performing companies: Nylia Ballet Company and Nylia
-          Ballet Youth Company!
-          <br></br>
-          <br></br>
-        </p>
-        <a
-          id="description-event-button"
-          href="https://www.nyliaballetacademy.com/nyliaballetcompany"
-        >
-          <NyliaButton label={"COMPANY AUDITIONS"}></NyliaButton>
-        </a>
-        <br /> <br />
-        <b>COMPANY AUDITIONS: AUGUST 3, 2024</b>
-        <p>
-          All auditions will be held at Nylia Ballet Academy.
-          <br></br>
-          <br></br>
-          <b>Nylia Ballet Youth Company Auditions:</b> 10:30 am - 11:30 am
-          <br></br>
-          <b>Nylia Ballet Company Auditions:</b> 1:30 pm - 2:30 pm
-        </p>
-        <br /> <br />
-        <hr></hr>
-        <a
-          id="description-event-button"
-          href="https://www.nyliaballetacademy.com/performances"
-        >
-          <NyliaButton label={"DRACULA AUDITIONS"}></NyliaButton>
-        </a>
         <br></br>
+        <h1>DRACULA</h1>
+        <h4>10.20.2024</h4>
         <p>
           Experience the mesmerizing world of Dracula, a Ballet set in
           Transylvania. Dracula and his brides seek to regain their waning
@@ -93,17 +60,16 @@ function Home() {
           vampire's attempts to take Svetlana as his bride, the villagers
           ultimately band together to defeat Dracula, with Svetlana ultimately
           foiling the vampire's plan, leading to his demise.
-        </p>
+        </p>{" "}
         <br></br>
-        <b>DRACULA AUDITIONS: AUGUST 3, 2024</b>
-        <p>
-          All auditions will be held at Nylia Ballet Academy.
-          <br></br>
-          <br></br>
-          <b>Youth Cast Auditions:</b> 9 am - 10:30 am<br></br>
-          <b>Mandatory Parent Meeting:</b> 9 am - 9:30 am<br></br>
-          <b>Adult Cast Auditions:</b> 12 pm - 1:30 pm
-        </p>
+        <a
+          id="description-event-button"
+          href="https://www.ticketmaster.com/dracula-el-paso-texas-10-20-2024/event/0C00610DCC433581"
+        >
+          <NyliaButton label={"DRACULA TICKETS"}></NyliaButton>
+        </a>
+        <br></br>
+        <br></br>
       </div>
     </div>
   );
