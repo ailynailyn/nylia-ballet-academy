6e5128327b7b6657c9fa964e2f3d325ece71b509
Ailyn Aguirre <ailyndarlenne@gmail.com>
2025-03-26 10:22:55 -0600

    Update schedule

--- src/components/Classes.js
+++ src/components/Classes.js
@@ -159,6 +159,8 @@ function Classes() {
       classSched2: "4:30 - 5:30 PM",
       classSched3: "SATURDAY",
       classSched4: "9:00 - 10:00 AM",
+      classSched5: "",
+      classSched6: "",
       classDesc:
         "Designed for 4 year olds. Students are introduced to ballet vocabulary and class etiquette, focusing on the development of coordination, strength, flexibility, and musicality during their early development stages. It is important to us to maintain a creative and accepting learning environment to keep our tiny dancers motivated and engaged in the art of ballet.",
       classRgstr: rgstrRqd,
@@ -173,6 +175,8 @@ function Classes() {
       classSched2: "4:30 - 5:30 PM",
       classSched3: "SATURDAY",
       classSched4: "10:00 - 11:00 AM",
+      classSched5: "",
+      classSched6: "",
       classDesc:
         "Designed for 5-6 year olds. Pre-Ballet II is a class designed to introduce the fundamentals of classical ballet technique. The class begins with conditioning, strengthening and stretching exercises to help students use, and isolate, certain muscle groups pertaining to dance. This class will enhance the student’s coordination, balance and flexibility, as well as musicality.",
       classRgstr: rgstrRqd,
@@ -187,6 +191,8 @@ function Classes() {
       classSched2: "4:30 - 5:30 PM",
       classSched3: "",
       classSched4: "",
+      classSched5: "",
+      classSched6: "",
       classDesc:
         "Ballet I will include basic barre exercises, as well as center work implementing the study of classical ballet vocabulary. Instruction involves strong concentration on correct body alignment, along with the development of attention span, discipline, and musicality.",
       classRgstr: rgstrRqd,
@@ -201,6 +207,8 @@ function Classes() {
       classSched2: "4:30 - 5:30 PM",
       classSched3: "",
       classSched4: "",
+      classSched5: "",
+      classSched6: "",
       classDesc:
         "Ballet II is for students who have studied the basics and are ready to concentrate on developing a broader ballet vocabulary through barre work, center exercises, and across the floor combinations designed to improve coordination, flexibility, endurance, and musicality. This class will strengthen the feet and ankles to prepare the students for pointe shoes, and more advanced technique.",
       classRgstr: rgstrRqd,
@@ -215,6 +223,8 @@ function Classes() {
       classSched2: "4:30 - 5:30 PM",
       classSched3: "WEDNESDAY",
       classSched4: "4:30 - 6:00 PM",
+      classSched5: "",
+      classSched6: "",
       classDesc:
         "Ballet III is an intermediate class designed with focus on classical ballet technique through a professional class progression. We highly recommend students at this level take class at least three times a week for optimal growth. Students will build on their skills and strength, and refine their movement with attention to technique, execution and artistry.",
       classRgstr: rgstrRqd,
@@ -225,10 +235,12 @@ function Classes() {
       classImg: ballet4Sm,
       imgSet: `${ballet4Sm} 300w, ${ballet4Md} 768w, ${ballet4Lg} 1280w, ${ballet4XL} 2000w`,
       webpSet: `${ballet4SmWebp} 300w, ${ballet4MdWebp} 768w, ${ballet4LgWebp} 1280w, ${ballet4XLWebp} 2000w`,
-      classSched1: "TUESDAY & THURSDAY",
-      classSched2: "5:30 - 7:00 PM",
-      classSched3: "",
-      classSched4: "",
+      classSched1: "MONDAY",
+      classSched2: "6:00 - 7:30 PM",
+      classSched3: "THURSDAY",
+      classSched4: "5:30 - 7:00 PM",
+      classSched5: "",
+      classSched6: "",
       classDesc:
         "Ballet IV is an advanced-level class with a required placement class prior to enrollment. Students continue developing advanced technique with a strong focus on musicality, discipline, and professionalism. At this advanced level, students are given extended barre exercises that incorporate increasing demi-pointe work, as well as stretching and strengthening exercises. Classical ballet lines should be well established, and students are introduced to new vocabulary for traveling steps, elevation, and pirouettes. The use of arms and upper back continues to build on the foundation established in the previous level, further refining their expression and fluidity.<br /><br />Students at this level should have a strong grasp of classical ballet vocabulary and can now adequately demonstrate correct use of line and musical performance in their movements.<br /><br /><i>We highly recommend that students take class at least four times a week for optimal growth.<i/>",
       classRgstr: rgstrRqd,
@@ -243,6 +255,8 @@ function Classes() {
       classSched2: "5:30 - 7:00 PM",
       classSched3: "",
       classSched4: "",
+      classSched5: "",
+      classSched6: "",
       classDesc:
         "Ballet V is an advanced-level class with a required placement class prior to enrollment. Students must demonstrate a thorough understanding of all positions, arabesque, and épaulement placement, as well as mastery of pirouette preparation and execution. Proper technique is critical at this level—students must show no signs of sickling, hip raising, or loss of control over their body, core, and limbs.<br/><br/>In this class, dancers continue to develop spatial awareness, body orientation, and full-body coordination, with an added focus on fluidity of movement. The curriculum includes advanced steps such as multiple pirouettes, turns in à la seconde, and beating jumps.<br/><br/><i>Material is taught quickly at this level, and students are expected to pick it up at a fast rate. We highly recommend that students take class at least four times a week for optimal growth.<i/>",
       classRgstr: rgstrRqd,
@@ -257,6 +271,8 @@ function Classes() {
       classSched2: "4:30 - 5:30 PM",
       classSched3: "",
       classSched4: "",
+      classSched5: "",
+      classSched6: "",
       classDesc:
         "Pointe I is an invitation-only class for select Ballet III and IV students. This class serves as a preparation for pointe work and requires students to demonstrate very strong ankles, excellent body awareness, core stability, and a solid foundation in basic classical ballet technique. Emphasis is placed on ensuring that students are physically prepared to safely begin pointe work, with a focus on proper alignment, strength, and control to prevent injury.",
       classRgstr: rgstrRqd,
@@ -271,6 +287,8 @@ function Classes() {
       classSched2: "5:30 - 6:30 PM",
       classSched3: "",
       classSched4: "",
+      classSched5: "",
+      classSched6: "",
       classDesc:
         "Pointe II is designed for students who have successfully completed Pointe I and have demonstrated the necessary strength, technique, and control to advance their pointe work. This class continues to build on the foundation established in Pointe I, focusing on increasing strength, stability, and endurance en pointe. Students will work on more complex combinations at the barre and in center, incorporating elements such as sustained balances, turns, and linking steps.<br/><br/>A strong emphasis is placed on precision, alignment, and fluidity of movement as students begin to explore more advanced pointe repertoire. In addition, this class prepares students for more demanding choreography, requiring a heightened sense of body awareness, coordination, and musicality. Safety remains a priority as students advance in their technical abilities.",
 
@@ -283,40 +301,14 @@ function Classes() {
       imgSet: `${pointe3Sm} 300w, ${pointe3Md} 768w, ${pointe3Lg} 1280w, ${pointe3XL} 2000w`,
       webpSet: `${pointe3SmWebp} 300w, ${pointe3MdWebp} 768w, ${pointe3LgWebp} 1280w, ${pointe3XLWebp} 2000w`,
       classSched1: "FRIDAY",
-      classSched2: "6:30 - 8:00 PM",
+      classSched2: "6:30 - 7:30 PM",
       classSched3: "",
       classSched4: "",
+      classSched5: "",
+      classSched6: "",
       classDesc:
         "Pointe III is for students who have progressed beyond Pointe II and are ready for more demanding work en pointe. This class focuses on developing greater stamina, precision, and artistry. Students will work on intricate combinations at the barre and in the center, incorporating more challenging turns, extended balances, and petit allegro movements en pointe.<br/><br/>The class emphasizes seamless transitions between steps, improving foot articulation and strength, and expanding the range of movement en pointe. Students also begin to focus on performance quality and expression, integrating musicality and dynamics into their pointe work. Pointe III prepares dancers for pre-professional level pointe technique, with attention to both technical and artistic development.",
       classRgstr: rgstrRqd,
-    },
-    pointeAdv: {
-      classTitle: "ADVANCED POINTE",
-      classAges: "Placement by Evaluation",
-      classImg: advPointeSm,
-      imgSet: `${advPointeSm} 300w, ${advPointeMd} 768w, ${advPointeLg} 1280w, ${advPointeXL} 2000w`,
-      webpSet: `${advPointeSmWebp} 300w, ${advPointeMdWebp} 768w, ${advPointeLgWebp} 1280w, ${advPointeXLWebp} 2000w`,
-      classSched1: "FRIDAY",
-      classSched2: "6:00 - 7:30 PM",
-      classSched3: "",
-      classSched4: "",
-      classDesc:
-        "Advanced Pointe is the highest level of pointe training, designed for experienced dancers who have mastered the technical requirements of pointe work and are ready to refine their performance abilities. This class pushes students to achieve greater control, strength, and fluidity in advanced movements. Dancers will execute complex sequences with multiple pirouettes, sustained balances, and intricate footwork, both at the barre and in the center.<br/><br/>There is a strong emphasis on the artistry of pointe work, with students expected to demonstrate a high level of musicality, grace, and expression in their movements. Dancers in Advanced Pointe will work on classical variations and repertoire, preparing for performances or auditions. This class also focuses on building the endurance necessary for full-length ballets and complex pointe choreography.",
-      classRgstr: rgstrRqd,
-    },
-    absBegAdultBallet: {
-      classTitle: "ADULT BALLET",
-      classAges: "- Absolute Beginner -",
-      classImg: begAdultBalletSm,
-      imgSet: `${begAdultBalletSm} 300w, ${begAdultBalletMd} 768w, ${begAdultBalletLg} 1280w, ${begAdultBalletXL} 2000w`,
-      webpSet: `${begAdultBalletSmWebp} 300w, ${begAdultBalletMdWebp} 768w, ${begAdultBalletLgWebp} 1280w, ${begAdultBalletXLWebp} 2000w`,
-      classSched1: "FRIDAY",
-      classSched2: "7:30 - 8:30 PM",
-      classSched3: "",
-      classSched4: "",
-      classDesc:
-        "Learn the fundamentals of the ballet technique in a welcoming and supportive environment! This workshop is designed for students to learn the basics of ballet terminology and class etiquette through detailed explanations and practice. Students will gain an understanding of correct body alignment, footwork coordination, and musicality while dancing easy-to-follow combinations.",
-      classRgstr: dropInWlcm,
     },
     begAdultBallet: {
       classTitle: "ADULT BALLET",
@@ -324,10 +316,12 @@ function Classes() {
       classImg: begAdultBalletSm,
       imgSet: `${begAdultBalletSm} 300w, ${begAdultBalletMd} 768w, ${begAdultBalletLg} 1280w, ${begAdultBalletXL} 2000w`,
       webpSet: `${begAdultBalletSmWebp} 300w, ${begAdultBalletMdWebp} 768w, ${begAdultBalletLgWebp} 1280w, ${begAdultBalletXLWebp} 2000w`,
-      classSched1: "TUESDAY & THURSDAY",
-      classSched2: "7:00 - 8:00 PM",
-      classSched3: "",
-      classSched4: "",
+      classSched1: "MONDAY",
+      classSched2: "7:30 - 8:30 PM",
+      classSched3: "TUESDAY & THURSDAY",
+      classSched4: "7:00 - 8:00 PM",
+      classSched5: "",
+      classSched6: "",
       classDesc:
         "Come enjoy the beauty and grace of classical ballet! This class will cover basic ballet positions, steps and vocabulary with a focus on the student's awareness of proper posture and body alignment. Standing barre work and center floor exercises will be given to enhance coordination, strength, balance and flexibility. Participants will also develop a sense of musical awareness and movement quality. No prior ballet experience necessary. ",
       classRgstr: dropInWlcm,
@@ -338,10 +332,12 @@ function Classes() {
       classImg: adultBalletSm,
       imgSet: `${adultBalletSm} 300w, ${adultBalletMd} 768w, ${adultBalletLg} 1280w, ${adultBalletXL} 2000w`,
       webpSet: `${adultBalletSmWebp} 300w, ${adultBalletMdWebp} 768w, ${adultBalletLgWebp} 1280w, ${adultBalletXLWebp} 2000w`,
-      classSched1: "MONDAY & WEDNESDAY",
-      classSched2: "7:00 - 8:30 PM",
-      classSched3: "SATURDAY",
-      classSched4: "12:00 - 1:30 PM",
+      classSched1: "MONDAY",
+      classSched2: "7:30 - 8:30 PM",
+      classSched3: "TUESDAY",
+      classSched4: "5:30 - 7:00 PM",
+      classSched5: "WEDNESDAY",
+      classSched6: "7:00 - 8:30 PM",
       classDesc:
         "This class is designed for intermediate ballet dancers. Participants will strengthen their ballet technique aimed to improve alignment and balance, strengthen muscles, and increase flexibility.",
       classRgstr: dropInWlcm,
@@ -352,38 +348,44 @@ function Classes() {
       classImg: advAdultSm,
       imgSet: `${advAdultSm} 300w, ${advAdultMd} 768w, ${advAdultLg} 1280w, ${advAdultXL} 2000w`,
       webpSet: `${advAdultSmWebp} 300w, ${advAdultMdWebp} 768w, ${advAdultLgWebp} 1280w, ${advAdultXLWebp} 2000w`,
-      classSched1: "MONDAY & THURSDAY",
+      classSched1: "TUESDAY",
       classSched2: "5:30 - 7:00 PM",
-      classSched3: "SATURDAY",
-      classSched4: "12:00 - 1:30 PM",
+      classSched3: "",
+      classSched4: "",
+      classSched5: "",
+      classSched6: "",
       classDesc:
         "This class is designed for advanced ballet dancers. Extensive prior ballet experience is required. We will focus on classical ballet technique through a professional class progression. Barre work, center and across the floor exercises are designed to better your technique, as well as performance quality and confidence. With quality and knowledgeable teaching, and an encouraging environment, these classes will be a perfect fit for those who aspire to improve their dancing, and artistry!",
       classRgstr: dropInWlcm,
     },
-    // adultAllLevelsBallet: {
-    //   classTitle: "ADULT BALLET",
-    //   classAges: "- All Levels -",
-    //   classImg: allAdultSm,
-    //   imgSet: `${allAdultSm} 300w, ${allAdultMd} 768w, ${allAdultMd} 1280w, ${allAdultMd} 2000w`,
-    //   webpSet: `${allAdultSm} 300w, ${allAdultMd} 768w, ${allAdultMd} 1280w, ${allAdultMd} 2000w`,
-    //   classSched1: "TUESDAY & THURSDAY",
-    //   classSched2: "10:00 - 11:30 AM",
-    //   classSched3: "",
-    //   classSched4: "",
-    //   classDesc:
-    //     "These class is for all dancers, regardless of any past experience! Our instructor caters to a broad spectrum of students. Whether you become a dedicated student who graduates to pointe work, or you’re content to fly under the radar, each level has it’s own benefits and challenges. No matter what level you’re in, remember, the ultimate goal of adult ballet class is to have fun!",
-    //   classRgstr: dropInWlcm,
-    // },
+    adultAllLevelsBallet: {
+      classTitle: "ADULT BALLET",
+      classAges: "- All Levels -",
+      classImg: allAdultSm,
+      imgSet: `${allAdultSm} 300w, ${allAdultMd} 768w, ${allAdultMd} 1280w, ${allAdultMd} 2000w`,
+      webpSet: `${allAdultSm} 300w, ${allAdultMd} 768w, ${allAdultMd} 1280w, ${allAdultMd} 2000w`,
+      classSched1: "SATURDAY",
+      classSched2: "12:00 - 1:30 PM",
+      classSched3: "",
+      classSched4: "",
+      classSched5: "",
+      classSched6: "",
+      classDesc:
+        "These class is for all dancers, regardless of any past experience! Our instructor caters to a broad spectrum of students. Whether you become a dedicated student who graduates to pointe work, or you’re content to fly under the radar, each level has it’s own benefits and challenges. No matter what level you’re in, remember, the ultimate goal of adult ballet class is to have fun!",
+      classRgstr: dropInWlcm,
+    },
     flexibility: {
       classTitle: "FLEXIBILITY",
       classAges: "- All Ages -",
       classImg: flexibilitySm,
       imgSet: `${flexibilitySm} 300w, ${flexibilityMd} 768w, ${flexibilityLg} 1280w, ${flexibilityXL} 2000w`,
       webpSet: `${flexibilitySmWebp} 300w, ${flexibilityMdWebp} 768w, ${flexibilityLgWebp} 1280w, ${flexibilityXLWebp} 2000w`,
-      classSched1: "MONDAY & WEDNESDAY",
+      classSched1: "WEDNESDAY",
       classSched2: "6:00 PM - 7:00 PM",
       classSched3: "SATURDAY",
       classSched4: "11:00 AM - 12:00 PM",
+      classSched5: "",
+      classSched6: "",
       classDesc:
         "Flexibility classes introduce various techniques of flexibility training as well as techniques that are especially designed to improve your range of movement and mobility. The focus of the class is to stretch and lengthen the muscles of the body, and to strengthen them to be able to better execute ballet technique or simply utilize in one’s own daily routine.",
       classRgstr: dropInWlcm,
@@ -411,6 +413,10 @@ function Classes() {
       classInfo[curClass].classSched3;
     document.getElementById("cur-class-sched4").innerHTML =
       classInfo[curClass].classSched4;
+    document.getElementById("cur-class-sched5").innerHTML =
+      classInfo[curClass].classSched5;
+    document.getElementById("cur-class-sched6").innerHTML =
+      classInfo[curClass].classSched6;
     document.getElementById("cur-class-rgstr").innerHTML =
       classInfo[curClass].classRgstr;
     document.getElementById("cur-class-description").innerHTML =
@@ -499,12 +505,6 @@ function Classes() {
               </Dropdown.Item>
               <Dropdown.Item
                 as="button"
-                onClick={() => setClassInfo("pointeAdv")}
-              >
-                Advanced Pointe
-              </Dropdown.Item>
-              <Dropdown.Item
-                as="button"
                 onClick={() => setClassInfo("begAdultBallet")}
               >
                 Adult Ballet - Beg
@@ -565,6 +565,8 @@ function Classes() {
             <p id="cur-class-sched2">{classInfo.preBallet1.classSched2}</p>
             <p id="cur-class-sched3">{classInfo.preBallet1.classSched3}</p>
             <p id="cur-class-sched4">{classInfo.preBallet1.classSched4}</p>
+            <p id="cur-class-sched5">{classInfo.preBallet1.classSched5}</p>
+            <p id="cur-class-sched6">{classInfo.preBallet1.classSched6}</p>
             <p id="cur-class-rgstr">{classInfo.preBallet1.classRgstr}</p>
           </div>
         </div>
@@ -664,13 +666,6 @@ function Classes() {
                   onClick={() => setClassInfo("pointe3")}
                 >
                   Pointe III
-                </Dropdown.Item>
-                <Dropdown.Item
-                  eventKey="2"
-                  as="button"
-                  onClick={() => setClassInfo("pointeAdv")}
-                >
-                  Advanced Pointe
                 </Dropdown.Item>
               </DropdownButton>
               <DropdownButton
