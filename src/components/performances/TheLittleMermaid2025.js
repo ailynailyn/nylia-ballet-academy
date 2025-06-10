515209250fb914f4037e133f64ceea52b4a933bf
Ailyn Aguirre <ailyndarlenne@gmail.com>
2025-06-10 16:19:41 -0600

    Little Mermaid schedule and cast list

--- src/components/performances/TheLittleMermaid2025.js
+++ src/components/performances/TheLittleMermaid2025.js
@@ -4,6 +4,8 @@ import Footer from "../Footer";
 
 // Files used
 import littleMermaidHandbook from "../../assets/performances/TheLittleMermaid2025/TheLittleMermaidHandbook2025.pdf";
+import littleMermaidRehearsalSchedule from "../../assets/performances/TheLittleMermaid2025/TheLittleMermaid2025RehearsalSchedule.pdf";
+import littleMermaidCastList from "../../assets/performances/TheLittleMermaid2025/TheLittleMermaid2025CastList.pdf";
 
 // IMAGES FOR PAGE
 import perfSect2ImgSm from "../../assets/performances/TheLittleMermaid2025/TheLittleMermaidPoster-300.png";
@@ -16,11 +18,27 @@ import PerformanceTemplate from "./templates/PerformanceTemplate";
 import "./TheLittleMermaid2025.css";
 
 function TheLittleMermaid2025() {
-  const status = "UPCOMING";
+  const status = "ACTIVE";
   const showName = "The Little Mermaid";
   const showDate = "2025";
   const headerDescription = `Dive into a world of sacrifice, love, and enchantment! Join the Nylia Ballet Company as we bring Hans Christian Andersen's timeless tale The Little Mermaid to life on stage. Witness the journey of a mermaid who dares to trade her voice for a chance at love, only to discover the true meaning of courage and selflessness.`;
   const handbook = littleMermaidHandbook;
+
+  const importantLinks = [
+    { buttonLabel: "Cast list", buttonLink: littleMermaidCastList },
+
+    {
+      buttonLabel: "rehearsal schedule",
+      buttonLink: littleMermaidRehearsalSchedule,
+    },
+    // {
+    //   buttonLabel: "Rehearsal Attire",
+    //   buttonLink: rehearsalReqs,
+    // },
+    // { buttonLabel: "Costume Requirements", buttonLink: costumeReqs },
+    // { buttonLabel: "communications", buttonLink: "" }, //todo
+    // { buttonLabel: "volunteers", buttonLink: "" }, //todo
+  ];
 
   const auditionDate = "Sunday, JUNE 8, 2025";
   // const perfSect2ButtonLink = "" TODO - Auditions Registry Link
@@ -42,6 +60,7 @@ function TheLittleMermaid2025() {
         // perfSect2ButtonLink={perfSect2ButtonLink}
         perfSect2Images={perfSect2Images}
         handbookLink={handbook}
+        importantLinks={importantLinks}
       />
       <Footer />
     </div>
